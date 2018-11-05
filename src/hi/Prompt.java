69494867af882a5c80154b21dd13b49eeb8f4ace
JonahtCheyette <jonahcheyette@gmail.com>
2018-11-04 18:36:23 -0800

    fixed all kinds of stuff

--- src/hi/Prompt.java
+++ src/hi/Prompt.java
@@ -36,17 +36,17 @@ public class Prompt {
 				}
 			} else {
 				System.out.print(text[text.length-1] + " (");
-				for(int i = 0; i < text.length-1; i++) {
-					if (i != 0) {
-						System.out.print("/" + text[i]);
+				for(int ik = 0; ik < text.length-1; ik++) {
+					if (ik != 0) {
+						System.out.print("/" + text[ik]);
 					} else {
-						System.out.print(text[i]);
+						System.out.print(text[ik]);
 					}
 				}
 				System.out.print(")\n");
 				this.playerChoice = input.nextLine();
-				for(int i = 0; i < text.length-1; i++) {
-					if (this.playerChoice.equalsIgnoreCase(text[i])) {
+				for(int il = 0; il < text.length-1; il++) {
+					if (this.playerChoice.equalsIgnoreCase(text[il])) {
 						this.checkChoice = true;
 					}
 				}
@@ -65,21 +65,21 @@ public class Prompt {
 		}
 		while (!this.checkChoice) {
 			System.out.print("What will " + this.player.getName() + " do? (");
-			for (int io = 0; io < choices[this.dDisplay].length; io++) {
-				if (choices[this.dDisplay][io] != null) {
-					if (io != 0) {
-						System.out.print("/" + choices[this.dDisplay][io]);
+			for (int im = 0; im < choices[this.dDisplay].length; im++) {
+				if (choices[this.dDisplay][im] != null) {
+					if (im != 0) {
+						System.out.print("/" + choices[this.dDisplay][im]);
 					} else {
-						System.out.print(choices[this.dDisplay][io]);
+						System.out.print(choices[this.dDisplay][im]);
 					}
 				}
 			}
 			System.out.print(")\n");
 			this.playerChoice = input.nextLine();
-			for (int io = 0; io < choices[0].length; io++) {
-				if (choices[0][io] != null) {
-					if (this.playerChoice.equalsIgnoreCase(choices[0][io])) {
-						this.choice = io;
+			for (int in = 0; in < choices[0].length; in++) {
+				if (choices[0][in] != null) {
+					if (this.playerChoice.equalsIgnoreCase(choices[0][in])) {
+						this.choice = in;
 						this.checkChoice = true;
 					}
 				}
