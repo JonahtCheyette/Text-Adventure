6f7ba19fa492a045345588d74cfe9024647bdc2f
JonahtCheyette <jonahcheyette@gmail.com>
2018-11-14 22:23:13 -0800

    added template for status effects

---
+++ src/hi/StatusEffect.java
@@ -0,0 +1,24 @@
+package hi;
+
+public class StatusEffect {
+	static Boolean started;
+	static Boolean endCondition;
+	
+	StatusEffect(){
+		
+	}
+	
+	static void start() {
+		
+	}
+	
+	static void applyEffect() {
+		
+	}
+	
+	
+	//not sure If I'll need this one.
+	static void end(){
+		
+	}
+}
